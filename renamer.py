d0a0dd75d0ebcf03f4b132e0073a30ab32759a3f
Andrei <askopelevich@gmail.com>
2018-07-14 22:46:46 -0700

    more changes

--- renamer.py
+++ renamer.py
@@ -1,7 +1,7 @@
 import os
-os.chdir('/Users/askopelevich/Music/iTunes/iTunes Media/Music')
 rename = []
-#get list of artist folders
+os.chdir('/Users/askopelevich/Music/iTunes/iTunes Media/Music')
+for fo in os.listdir(): #get list of artist folders
+    print(fo)
 #for each artist folder, list of album folders
 #for each album, get names of files, add files w/ ' - ' to list
-print(os.getcwd())
