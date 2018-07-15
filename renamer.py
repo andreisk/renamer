36c03e75a06369ebfbdc457e65997c743bf022db
Andrei <askopelevich@gmail.com>
2018-07-14 23:25:36 -0700

    loops

--- renamer.py
+++ renamer.py
@@ -1,7 +1,24 @@
 import os
+import re
+
 rename = []
-os.chdir('/Users/askopelevich/Music/iTunes/iTunes Media/Music')
+path = '/Users/askopelevich/Music/iTunes/iTunes Media/Music'
+os.chdir(path)
 for fo in os.listdir(): #get list of artist folders
-    print(fo)
-#for each artist folder, list of album folders
-#for each album, get names of files, add files w/ ' - ' to list
+    if fo != '.DS_Store':
+        print('Artist:')
+        os.chdir(fo)
+        print(os.getcwd())
+        for al in os.listdir():  #for each artist folder, list of album folders
+            if al != '.DS_Store':
+                print('Album:')
+                os.chdir(al)
+                print(os.getcwd())
+                for so in os.listdir(): #for each album, get names of files
+                    if so != '.DS_Store':
+                        print('Song')
+                        print(so.encode('utf-8'))
+                        #add files w/ ' - ' to list to make sure it works
+                        #rename files
+                os.chdir('..')
+        os.chdir('..')
